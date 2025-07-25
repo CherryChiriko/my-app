3b17c45ca3c2ed3b7ba7e23fead7161cda0a27ef
CherryChiriko <sofia.digennarox@gmail.com>
2025-07-25 10:53:31 +0200

    Update on routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import { useSelector } from "react-redux";
 import { selectActiveTheme } from "./features/themeSlice";
 import Navbar from "./components/Navbar";
+import DeckManager from "./components/DeckManager";
+import StudySession from "./components/StudySession";
+import SettingsPage from "./components/SettingsPage";
+import Dashboard from "./components/Dashboard";
+import Fourzerofour from "./components/404";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const activeTheme = useSelector(selectActiveTheme);
@@ -14,7 +20,17 @@ function App() {
 
   return (
     <div style={appContainerStyles}>
-      <Navbar />
+      {/* <Navbar /> */}
+      {/* Render different views based on currentView state */}
+      <main className="container mt-3"> {/* Use a container for page content */}
+        <Routes> {/* Define your routes here */}
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/decks" element={<DeckManager />} />
+          <Route path="/study" element={<StudySession />} />
+          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="*" element={<Fourzerofour />} />
+        </Routes>
+      </main>
     </div>
   );
 }
---
+++ src/components/404.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Fourzerofour = () => {
+  return <h1>Fourzerofour</h1>;
+};
+export default Fourzerofour;
